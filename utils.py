a577ad9db6666dcf0575bc65294dc543602b9eb9
Ming-Yuan Yu <3705281+tn00364361@users.noreply.github.com>
2022-09-30 00:43:07 -0400

    update network

--- utils.py
+++ utils.py
@@ -1,40 +1,37 @@
 
 import logging
-from functools import partial
-
 import torch
 
 
-class SimpleCNN(torch.nn.Module):
-    def __init__(self, in_channels: int = 1, c0: int = 32, kernel_size: int = 3):
+class ConvBN2d(torch.nn.Module):
+    def __init__(self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int = 3
+        ) -> None:
         super().__init__()
 
-        self.in_channels = in_channels
-
-        _conv2d = partial(
-            torch.nn.Conv2d,
-            kernel_size=kernel_size,
+        self.conv = torch.nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
             stride=2,
             padding=kernel_size // 2,
             padding_mode='circular',
             bias=False
         )
+        self.bn = torch.nn.BatchNorm2d(out_channels)
 
-        # [n, in_channels, 32, 32] -> [n, in_channels * 4, 16, 16]
-        # -> [n, c0, 8, 8] -> [n, 2 * c0, 4, 4] -> [n, 4 * c0, 2, 2]
-        self.layers = torch.nn.ModuleList()
-        c_in = self.in_channels * 4
-        for k in range(3):
-            c_out = 2**k * c0
-            self.layers.append(_conv2d(c_in, c_out))
-            self.layers.append(torch.nn.BatchNorm2d(c_out))
-            self.layers.append(torch.nn.ReLU())
-            self.layers.append(torch.nn.Dropout2d())
-            c_in = c_out
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.bn(self.conv(x))
 
-        # [n, 8 * c0, 2, 2] -> [n, 32 * c0] -> [n, 10]
-        self.layers.append(torch.nn.Flatten())
-        self.layers.append(torch.nn.Linear(16 * c0, 10))
+
+class DWT2(torch.nn.Module):
+    def __init__(self) -> None:
+        """
+        @brief      2D discrete wavelet transform w/ Haar basis
+        """
+        super().__init__()
 
         self.haar_basis = 0.5 * torch.tensor([
             [+1, +1, +1, +1],
@@ -43,18 +40,44 @@ class SimpleCNN(torch.nn.Module):
             [+1, -1, -1, +1]
         ], dtype=torch.float32, requires_grad=False).unsqueeze(0)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.haar_basis != x.device:
             self.haar_basis = self.haar_basis.to(x.device)
 
-        h2, w2 = x.shape[-2] // 2, x.shape[-1] // 2
+        n, c, h, w = x.shape
+        h2, w2 = h // 2, w // 2
 
-        y = x.reshape(-1, self.in_channels, h2, 2, w2, 2).permute(0, 3, 5, 1, 2, 4)
-        y = torch.matmul(
-            self.haar_basis,
-            y.reshape(-1, 4, self.in_channels * h2 * w2)
-        )
-        y = y.reshape(-1, self.in_channels * 4, h2, w2)
+        y = x.reshape(n, c, h2, 2, w2, 2).permute(0, 3, 5, 1, 2, 4)
+        y = torch.matmul(self.haar_basis, y.reshape(n, 4, c * h2 * w2))
+
+        return y.reshape(n, c * 4, h2, w2)
+
+
+class SimpleCNN(torch.nn.Module):
+    def __init__(self, c0: int = 32) -> None:
+        super().__init__()
+
+        # [n, 1, 32, 32] -> [n, 4, 16, 16]
+        self.dwt2 = DWT2()
+
+        # [n, 4, 16, 16] -> [n, c0, 8, 8] -> [n, 2 * c0, 4, 4] -> [n, 4 * c0, 2, 2]
+        self.layers = torch.nn.ModuleList()
+        c_in = 4
+        for k in range(3):
+            c_out = 2**k * c0
+            self.layers.append(ConvBN2d(c_in, c_out))
+            self.layers.append(torch.nn.ReLU())
+            self.layers.append(torch.nn.Dropout2d())
+            c_in = c_out
+
+        # [n, 4 * c0, 2, 2] -> [n, 16 * c0] -> [n, 10]
+        self.layers.append(torch.nn.Flatten())
+        self.layers.append(torch.nn.Linear(16 * c0, 10))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        y = self.dwt2(x)
+
+        # regular convolutional layers
         for layer in self.layers:
             y = layer(y)
 
